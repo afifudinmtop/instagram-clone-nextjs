3454bc337c31ecf1d10c653e4c837d4a541a7c21
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2024-01-22 17:42:10 +0700

    v1.4.0
    login API

--- instagram/app/register/page.js
+++ instagram/app/register/page.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Register = () => {
   const [username, set_username] = useState("");
--- instagram/server.js
+++ instagram/server.js
@@ -119,11 +119,34 @@ nextApp.prepare().then(() => {
     }
   });
 
-  // Rute Login
-  app.get("/api/login", (req, res) => {
-    // Contoh pengaturan user ke sesi
-    req.session.user = { name: "User1", isLoggedIn: true };
-    res.send("Anda telah login");
+  // login
+  app.post("/api/login/", async (req, res) => {
+    try {
+      const username = req.body.username;
+      const password = req.body.password;
+
+      // check existing username
+      pool.query(
+        "SELECT * FROM user WHERE username = ?",
+        [username],
+        (error, results, fields) => {
+          if (results.length < 1) {
+            return res.json({ pesan: "user not found!" });
+          } else {
+            const resultx = bcrypt.compareSync(password, results[0].password);
+            if (resultx) {
+              req.session.user = { uuid: results[0].uuid, isLoggedIn: true };
+              return res.json({ pesan: "sukses!" });
+            } else {
+              return res.json({ pesan: "wrong password!" });
+            }
+          }
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Server error");
+    }
   });
 
   // Rute Logout
