3f86fbbe5fa8ee65dcbcb477c914c1aaac2c10ac
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2024-01-26 11:26:33 +0700

    v1.19.0
    button di profile

--- instagram/app/profil/Profil_button.js
+++ instagram/app/profil/Profil_button.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 const Profil_button = () => {
   return (
     <div className="flex justify-between gap-[14px] mx-[16px] mt-[8px]">
-      <div className="bg-[#1877F2] text-white text-[10px] text-center rounded h-[25px] w-1/2 leading-[25px]">
-        Follow
-      </div>
-      <div className="bg-[#DFDFDF] text-black text-[10px] text-center rounded h-[25px] w-1/2 leading-[25px]">
-        Message
+      <Link
+        href="/setting"
+        className="cursor-pointer flex items-center justify-center bg-[#DFDFDF] text-black text-[10px] text-center rounded h-[25px] w-1/2"
+      >
+        <span>Edit Profil</span>
+      </Link>
+
+      <div className="cursor-pointer flex items-center justify-center bg-[#DFDFDF] text-black text-[10px] text-center rounded h-[25px] w-1/2">
+        <span>Share Profil</span>
       </div>
     </div>
   );
--- instagram/app/profil/Profil_header.js
+++ instagram/app/profil/Profil_header.js
@@ -1,20 +1,24 @@
+import Link from "next/link";
+
 const Profil_header = () => {
   return (
     <div className="flex justify-between ms-[20px] me-[25px]">
       <div className="flex my-auto">
-        <img
-          src="/arrow_left.png"
-          className="w-[10px] h-[20px] my-auto me-[15px]"
-        />
+        <Link href="/home" className="my-auto">
+          <img
+            src="/arrow_left.png"
+            className="w-[10px] h-[20px] my-auto me-[15px]"
+          />
+        </Link>
 
         <div className="text-xl font-bold text-black mb-[5px] me-[5px]">
           messi.official
         </div>
       </div>
 
-      <div className="my-auto">
+      <Link href="/setting" className="my-auto">
         <img src="/titik3.png" className="w-[3px] h-[12px] my-auto" />
-      </div>
+      </Link>
     </div>
   );
 };
